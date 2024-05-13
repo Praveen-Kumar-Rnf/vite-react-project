f221185f898b35ed44ea7639b8acfbb17f8a598f
Praveen-Kumar-Rnf <praveen.kumar@rnf.tech>
2024-05-13 12:37:53 +0530

    update

--- src/Components/nav/Footer.jsx
+++ src/Components/nav/Footer.jsx
@@ -1,16 +1,51 @@
-import React from 'react'
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
 export default function Footer() {
   return (
-    <div className='footer'>
-      <div className='container'>
-        <div className="row text-center">
-          <div className="col-md-12">
-            <p>© 2024 Copyright. All Rights Reserved</p>
+    <section className="footer">
+      <div className="container mb-5 pt-5 footer-menu links">
+        <div className="row footer-nav">
+          <div className="col-md-3">
+            <div className="logos">
+              <i className="fab fa-medium-m fa-4x"></i><br/>
+              <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, veritatis?</span>
+            </div>
+          </div>
+          <div className="col-md-3 footer-menu">
+          <h3>Quick Links</h3>
+            <ul>
+            <li><i class="fa fa-angle-right"></i> Home</li>
+              <li><i class="fa fa-angle-right"></i> About Us</li>
+              <li><i class="fa fa-angle-right"></i> Blog</li>
+              <li><i class="fa fa-angle-right"></i> Services</li>
+              <li><i class="fa fa-angle-right"></i> Contact Us</li>
+            </ul>
+          </div>
+          <div className="col-md-3 footer-menu">
+            <h3>Useful Links</h3>
+            <ul>
+              <li><i class="fa fa-angle-right"></i> Home</li>
+              <li><i class="fa fa-angle-right"></i> About Us</li>
+              <li><i class="fa fa-angle-right"></i> Blog</li>
+              <li><i class="fa fa-angle-right"></i> Services</li>
+              <li><i class="fa fa-angle-right"></i> Contact Us</li>
+            </ul>
+          </div>
+          <div className="col-md-3 footer-menu">
+            <h3>Contact Info</h3>
+            <ul>
+              <li><i class="fa fa-phone" aria-hidden="true"></i><br/><a href="tel:567-891-2563">567-891-2563</a></li>
+              <li><i className="fa fa-envelope"></i><br/><a href="mailto:info@gmail.com">info@gmail.com</a></li>
+            </ul>
           </div>
         </div>
       </div>
-    </div>
-  )
+      <div className="copyright">
+        <div className="text-center">
+            <span>© 2024 Copyright. All Rights Reserved</span>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Bike from '../../assets/images/ninja.jpg';
+import "./Home.css";
 
 export default function Home() {
   return (
@@ -44,14 +45,142 @@ export default function Home() {
           </button>
         </div>
       </section>
-      <section className="">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6"></div>
-            <div className="col-md-6"></div>
+      <section className="client-logo">
+        <div className="container pt-5 pb-5">
+          <div className="row align-items-center h-100">
+            <div className="container rounded">
+              <h1 className="text-center">Featured in:</h1>
+              <div className="slider mt-5">
+                <div className="logos">
+                  <i className="fab fa-js fa-4x"></i>
+                  <i className="fab fa-linkedin-in fa-4x"></i>
+                  <i className="fab fa-dribbble fa-4x"></i>
+                  <i className="fab fa-medium-m fa-4x"></i>
+                  <i className="fab fa-github fa-4x"></i>
+                </div>
+                <div className="logos">
+                  <i className="fab fa-js fa-4x"></i>
+                  <i className="fab fa-linkedin-in fa-4x"></i>
+                  <i className="fab fa-dribbble fa-4x"></i>
+                  <i className="fab fa-medium-m fa-4x"></i>
+                  <i className="fab fa-github fa-4x"></i>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
+      <section className="about-us">
+          <div className="about-col">
+          <img src={Bike} className="d-block w-100" alt="..."></img>
+          </div>
+          <div className="about-col">
+            <div className="about-heading">
+              <h2>About Us</h2>
+              <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus iusto deleniti provident enim tempore distinctio obcaecati ipsam amet magnam accusantium? Expedita ipsa suscipit impedit obcaecati vitae repudiandae deserunt corporis. Eius et minus nisi facilis, rem doloremque non laborum sed vero iusto iure ad neque iste molestiae delectus amet id eaque deleniti voluptatem dicta? Qui facilis neque quam, itaque eum voluptatibus aspernatur assumenda iure sequi impedit excepturi nulla minus quasi, quaerat dicta dolores. Quibusdam maiores temporibus velit quia qui saepe, illum delectus sunt quaerat expedita esse consequatur eos nihil! Error corrupti repellendus voluptatibus odio perferendis in eos quam magnam nihil eveniet?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus iusto deleniti provident enim tempore distinctio obcaecati ipsam amet magnam accusantium? Expedita ipsa suscipit impedit obcaecati vitae repudiandae deserunt corporis. Eius et minus nisi facilis, rem doloremque non laborum sed vero iusto iure ad neque iste molestiae delectus amet id eaque deleniti voluptatem dicta? Qui facilis neque quam, itaque eum voluptatibus aspernatur assumenda iure sequi impedit excepturi nulla minus quasi, quaerat dicta dolores. Quibusdam maiores temporibus velit quia qui saepe, illum delectus sunt quaerat expedita esse consequatur eos nihil! Error corrupti repellendus voluptatibus odio perferendis in eos quam magnam nihil eveniet?</p>
+            <button type="button" class="btn btn-outline-primary">Read More</button>
+          </div>
+      </section>
+      <section className="card-section mt-5" >
+        <div className="container">
+          <h2 className='card-heading mb-5 text-center'>Header Title</h2>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="card text-bg-success mb-3">
+                <div className="card-header">Header</div>
+                <div className="card-body">
+                  <h5 className="card-title">Success card title</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card text-bg-success mb-3">
+                <div className="card-header">Header</div>
+                <div className="card-body">
+                  <h5 className="card-title">Success card title</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card text-bg-success mb-3">
+                <div className="card-header">Header</div>
+                <div className="card-body">
+                  <h5 className="card-title">Success card title</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className='blog-section mb-5 mt-5'>
+        <div className="container">
+        <h2 className='card-heading mb-5 text-center'>Blog Title</h2>
+          <div className="row row-cols-1 row-cols-md-3 g-4">
+            <div className="col">
+              <div className="card">
+                <img src={Bike} className="card-img-top" alt="..."></img>
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card">
+                <img src={Bike} className="card-img-top" alt="..."></img>
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card">
+                <img src={Bike} className="card-img-top" alt="..."></img>
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card">
+                <img src={Bike} className="card-img-top" alt="..."></img>
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card">
+                <img src={Bike} className="card-img-top" alt="..."></img>
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card">
+                <img src={Bike} className="card-img-top" alt="..."></img>
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
     </>
   )
 }
